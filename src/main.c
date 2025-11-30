b1b5bbeeded8d0178e7f2d536191a92357bc6058
Ayush Hegde <ayushhegde2005@gmail.com>
2025-12-01 03:39:22 +0530

    Enabled external command execution by implementing PATH lookup and process management with fork/execve.

--- src/main.c
+++ src/main.c
@@ -3,17 +3,44 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define INPUT_BUFFER_SIZE 100
 #define CMD_AVAILABLE 3 //specifies the number of command suported
 #define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGUMENT 100
 
 //specifing the list of commands the are supported/available to the shell
 //defining it as static const because the commands and their names are  fixed and available throughout the program
 static const char *builtin_cmd[CMD_AVAILABLE] = {"exit", "echo", "type"};
 
-//this is to implement the type command to check the eligiblity of the command
+//this function returns the path of the executable
+char *find_in_path(char *cmd){
+    if (strchr(cmd, '/')){
+        if (access(cmd, X_OK) == 0) return strdup(cmd);
+    }
 
+    char *paths = getenv("PATH");
+    if (!paths) return NULL;
+
+    char *path = strdup(paths);
+    char *state;
+    char *p = strtok_r(path, ":", &state);
+    
+    while (p != NULL){
+        char full_path[MAX_COMMAND_LENGTH];
+        snprintf(full_path, sizeof(full_path), "%s/%s", p, cmd);
+        if (access(full_path, X_OK) == 0){
+            free(path);
+            return strdup(full_path);
+        }
+        p = strtok_r(NULL, ":", &state);
+    }
+    return NULL;
+}
+
+//this is to implement the type command to check the eligiblity of the command
 void execute_type(char *input){
     char *token, *ptr = input;
     char *saveptr1; // State for the outer loop
@@ -31,23 +58,13 @@ void execute_type(char *input){
             printf("%s is a shell builtin\n", token);
         }
         else{
-            char *path = strdup(getenv("PATH"));
-            if(path != NULL){
-                char *saveptr2; // State for the inner loop
-                char *dir = strtok_r(path, ":", &saveptr2);
-                while(dir != NULL){
-                    char full_path[MAX_COMMAND_LENGTH];
-                    snprintf(full_path, sizeof(full_path), "%s/%s", dir, token);
-                    if(access(full_path, X_OK) == 0){
-                        printf("%s is %s\n", token, full_path);
-                        is_path_cmd = true;
-                        break;
-                    }
-                    dir = strtok_r(NULL, ":", &saveptr2);
-                }
+            char *path = find_in_path(token);
+            if (path != NULL){
+                printf("%s is %s\n", token, path);
+                is_path_cmd = true;
             }
-            free(path);
         }
+
         if (is_path_cmd == false && is_builtin_cmd == false){
             printf("%s: not found\n", token);
         }
@@ -56,7 +73,44 @@ void execute_type(char *input){
     }
 }
 
-int main(int argc, char *argv[]) {
+void execute_external_command(char *input, char **envp){
+    char *state, *arguments[MAX_ARGUMENT]; //saves the state of token
+    char *token = strtok_r(input, " ", &state);
+    int i = 0;
+
+    while (token != NULL){
+        arguments[i++] = token;
+        token = strtok_r(NULL, " ", &state);
+    }
+    arguments[i] = NULL;  //cause execve requires NULL terminated array
+
+    if (arguments[0] == NULL){
+        return;
+    }
+    
+    char *path = find_in_path(arguments[0]);
+    if (!path){
+        printf("%s: command not found\n", arguments[0]);
+        return;
+    }
+
+    pid_t pid = fork();
+
+    if (pid == 0){
+        //this is a child process
+        execve(path, arguments, envp);
+        perror("execve");
+        exit(1);
+    }
+    else {
+        // parent process waits
+        waitpid(pid, NULL, 0);
+    }
+
+    free(path);
+}
+
+int main(int argc, char *argv[], char *envp[]) {
 	// Flush after every printf
 	setbuf(stdout, NULL);
     // defining the buffer to store the input commands
@@ -85,6 +139,11 @@ int main(int argc, char *argv[]) {
         else if(strncmp(input, "type ", 5) == 0){
             char *ptr = input + 5;
             execute_type(ptr);
+        }
+
+        //implement executing commands through PATH and provide them with arguments
+        else if (input[0] != '\0'){
+            execute_external_command(input, envp);
         }
 
         else{
